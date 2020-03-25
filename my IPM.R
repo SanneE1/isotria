b7189d6d9d3b0ddec447a2103671ccc38c9b23c0
Sanne <sanne.evers@idiv.de>
2020-03-25 11:26:30 +0100

    Adding site to kernel

--- my IPM.R
+++ my IPM.R
@@ -5,55 +5,31 @@ library(ggplot2)
 library(testthat)
 library(lme4)
 library(bbmle)
-library(dplyr)
+library(plyr); library(dplyr)
 library(tidyr)
 library(lme4)
-
-
 options(stringsAsFactors = F)
+
 
 # read data 
 
-isotria_long <- read.csv("data/isotria_long.csv")
 # creating a dataframe (df) with logarithmic size_t0 and size_t1 called iso
 iso<-read.csv("data/isotria_long.csv") %>%
   subset(size_t0 != 0) %>%
-mutate(size_t0 = log(size_t0),
-       size_t1 = log(size_t1))
-
-View(iso)
-
-
-
-
-# Visualize data ---------------------------------------------------------------------
-
-#1. Plot survival, growth, flowerpropability, flowernumber, propabilyty to go dormant, propabilyty to go out of dormancy
-par( mfrow=c(2,2), mar = c(3.5,3.5,1,0.3), mgp = c(2,0.7,0) )
-plot(jitter(surv_t1) ~ size_t0, data = iso)
-plot(size_t1  ~ size_t0, data = iso )
-plot(flower_t1  ~ size_t0, data = iso)
-plot(n_flower_t1  ~ size_t0, data = iso)
-plot(dormancy_t1~(size_t0),data = iso)
-
-
-# Plot GLM ---------------------------------------------------------------
+  mutate(size_t0 = log(size_t0),
+         size_t1 = log(size_t1))
 
 
 # fit models 
 
-
 sr_mod  <- glmer(surv_t1 ~ size_t0 + (size_t0 | year_t1), data = iso %>% 
                              filter(!is.na(surv_t1) & (!is.na(size_t0))), family = 'binomial' )
-sr_mod
+
 
 # creating a dataframe that excludes size_t0 = 0  and size_t1 = 0 and allso makes size_t0 and size_t0 logarythmic
-iso_gr<-read.csv("data/isotria_long.csv") %>%
-  subset(size_t0 != 0) %>%
-  subset(size_t1 != 0)%>%
-  mutate(size_t0 = log(size_t0),
-         size_t1 = log(size_t1))
-iso_gr
+iso_gr<-iso %>%
+  subset(size_t1 > -999)
+
 gr_mod  <- lmer(size_t1 ~ size_t0 + (size_t0 | year_t1), data = iso_gr %>% 
               filter(!is.na(size_t1) & (!is.na(size_t0))))
 
@@ -67,67 +43,51 @@ flower_n_mod <-glmer(n_flower_t1 ~ size_t0 + Site + (1 | year_t1), data = iso %>
 
 dorm_mod<-glmer(dormancy_t1 ~ size_t0 * Site + (1 | year_t1),data = iso%>%
                   filter(!is.na(dormancy_t1) & (!is.na(size_t0))), family = 'binomial')
-dorm_mod
+
 
 
 # propability to get out of dormancy
 
 d <- read.csv('data/isotria_long.csv')
-
-head(d)
 dorm <- d[which( d$surv_t1 == 1),] 
-dorm$size_t0
-head(isotria_long)
+
 # quick check if there are individuals that remain in dormancy more than one year
 a <- ddply(dorm[which(!is.na(dorm$remain_dorm_t1)),], .(New_Tag), transform, duration_dormancy = cumsum(remain_dorm_t1))
-dorm$remain_dorm_t1
 max(a$duration_dorm)
-
 
 # get out of dormancy probability
 
 p_stay <- mean(dorm$remain_dorm_t1, na.rm = TRUE) 
-p_stay
 p_out <- 1 - p_stay
-p_out
-
 
 
 # Number of fruits per flower
 # Read and format data
-d <- read.csv('data/isotria_long.csv')
 flower_n <- d[complete.cases(d$size_t0, d$surv_t1, d$Habitat_Man),] %>%
   subset(size_t0 != 0)  %>%
   mutate(size_t0 = log(size_t0))
 # creating a dataframe that excludes zeros 
-f<-d %>% filter(!is.na(n_flower_t1)) 
 
-f
-g<- f %>%filter(n_flower_t1 !=0)  
-g
-head(g)
+g<-d %>% filter(!is.na(n_flower_t1) & n_flower_t1 != 0) 
+
+
 #creating a new collum that contains the number of fruits per flower
 #by deviding the number of fruits by the number of flowers
 g$n_fruits_per_flower<-g$n_fruit_t1/g$n_flower_t1
-g
+
 
 # getting the mean of friuts per flower
 fruiting<- mean(g$n_fruits_per_flower, na.rm=TRUE)
-fruiting
 
 # getting the mean and sd for the size distribution of new plants
-w1 <-isotria_long %>% filter(is.na(stage_t0) & stage_t1 == 'plant') 
-
-w2<-w %>%filter(!is.na(size_t1))
-new_plants_mean<-mean(w2$size_t1)
-new_plants_mean
-new_plants_sd<-sd(w2$size_t1)
-new_plants_sd
+w1 <-d %>% filter(is.na(stage_t0) & stage_t1 == 'plant' & !is.na(size_t1)) 
+new_plants_mean<-mean(w1$size_t1)
+new_plants_sd<-sd(w1$size_t1)
 
 # getting the mean and sd for the size distribution of woke plants
-k1<- isotria_long %>% filter(stage_t0=='dormant')
-l1<-k %>% filter(stage_t1=='plant')
-head(l1)
+k1<- d %>% filter(stage_t0=='dormant')
+l1<-k1 %>% filter(stage_t1=='plant')
+
 l2<-l1 %>%filter(!is.na(size_t1))
 woke_plants_mean<-mean(l2$size_t1)
 woke_plants_mean
@@ -162,31 +122,31 @@ x_seq <- seq(min(iso$size_t0, na.rm=T),
              max(iso$size_t0, na.rm=T), by = 0.1)
 
 #sr
-sr_b0<-coef(sr_mod)[1]
-sr_b1<-coef(sr_mod)[2]
+sr_b0<-fixef(sr_mod)[1]
+sr_b1<-fixef(sr_mod)[2]
 sr_y_pred<-(sr_b0 + sr_b1*(x_seq))
 
 #gr
-gr_b0<-coef(gr_mod)[1]
-gr_b1<-coef(gr_mod)[2]
-gr_y_pred<-(gr_b0*(x_seq))
+gr_b0<-fixef(gr_mod)[1]
+gr_b1<-fixef(gr_mod)[2]
+gr_y_pred<-(gr_b0 + gr_b1 *(x_seq))
 
 #flowpop_mod
-flp_b0<-coef(flowpop_mod)[1]
-flp_b1<-coef(flowpop_mod)[2]
-flp_y_pred<-exp(flp_b0+flp_b1*(x_seq))
+flp_b0<-fixef(flowpop_mod)[1]
+flp_b1<-fixef(flowpop_mod)[2]
+flp_y_pred<-exp(flp_b0+flp_b1*(x_seq))   ### this doesn't follow the model
 
 
 #flower_n_mod
-fln_b0<-coef(flower_n_mod)[1]
-fln_b1<-coef(flower_n_mod)[2]
-fln_y_pred<-exp(fln_b0+fln_b1*(x_seq))
+fln_b0<-fixef(flower_n_mod)[1]
+fln_b1<-fixef(flower_n_mod)[2]
+fln_y_pred<-exp(fln_b0+fln_b1*(x_seq))   ### this doesn't follow the model
 
 
 #dorm_mod
-do_b0<-coef(dorm_mod)[1]
+do_b0<-fixef(dorm_mod)[1]
 do_b1<-coef(dorm_mod)[2]
-do_y_pred<-exp(do_b0+do_b1*(x_seq))
+do_y_pred<-exp(do_b0+do_b1*(x_seq)) ### this doesn't follow the model
 
 
 # IPM functions -------------------------------------------------------------
@@ -201,10 +161,15 @@ pars  <- list( surv_b0 = fixef(sr_mod)[1],
                grow_sd = summary(gr_mod)$sigma,
                flop_b0 = fixef(flowpop_mod)[1],
                flop_b1 = fixef(flowpop_mod)[2],
+               flop_b2 = fixef(flowpop_mod)[3],
+               flop_b3 = fixef(flowpop_mod)[4],
                flon_b0 = fixef(flower_n_mod)[1],
                flon_b1 = fixef(flower_n_mod)[2],
+               flon_b2 = fixef(flower_n_mod)[3],
                dom_b0 = fixef(dorm_mod)[1],
                dom_b1 = fixef (dorm_mod)[2],
+               dom_b2 = fixef(dorm_mod)[3],
+               dom_b3 = fixef(dorm_mod)[4],
                p_stay = p_stay, 
                p_out = p_out,
                fruiting=fruiting,
@@ -216,13 +181,13 @@ pars  <- list( surv_b0 = fixef(sr_mod)[1],
                U       = max(iso$size_t0,na.rm=T),
                mat_siz = 50
 )
-(pars$surv_b0)
-pars
-# functions 
 
-# Transforms all values below/above limits in min/max size
+# functions 
 x<- seq(min(iso$size_t0),
         max(iso$size_t0), length.out = n)
+
+# Transforms all values below/above limits in min/max size
+
 x_range <- function(x,pars){
   pmin(pmax(x,pars$L),pars$U)
 }
@@ -251,94 +216,63 @@ pxy<-function(x,y,pars){
 
 
 # flowering at size x
-flx<-function(x,pars){
+flx<-function(x,pars, site){
   xb <- x_range(x, pars)
   # flowering  prob. of each x size class 
-  return( inv_logit(pars$flop_b0 + pars$flop_b1 * xb) )
+  return( inv_logit(pars$flop_b0 + 
+                    pars$flop_b1 * xb +
+                    pars$flop_b2 * site +
+                    pars$flop_b3 * xb * site) )
 }
 
 
 # flowernumber at size x
-fln<-function(x,pars){
+fln<-function(x, pars, site){
   xb <- x_range(x, pars)
   # flowernumber of each x size class 
-  return( exp(pars$flon_b0 + pars$flon_b1 * xb)) 
+  return( exp(pars$flon_b0 + pars$flon_b1 * xb + pars$flon_b2 * site)) 
 }
 
 
 # propability to go dormant at size x
-dox<-function(x,pars){
+dox<-function(x,pars, site){
   xb <- x_range(x, pars)
-  return( inv_logit(pars$dom_b0 + pars$dom_b1 * xb) )
+  return( inv_logit(pars$dom_b0 + 
+                      pars$dom_b1 * xb +
+                      pars$dom_b2 * site +
+                      pars$dom_b3 * site * xb) )
 }
 
 # New recruits from size x to size y
-nrxy <- function(y,pars){
+nrxy <- function(y,pars, h){
   
   # returns a *probability density distribution* for each x value
   return( dnorm(y, 
                 mean = pars$new_plants_mean , 
-                sd   = pars$new_plants_sd) )
+                sd   = pars$new_plants_sd) ) * h
 }
 
 # woke plants from size x to size y
-woxy <- function(y,pars){
+woxy <- function(y,pars, h){
   
   # returns a *probability density distribution* for each x value
   return( dnorm(y, 
                 mean = pars$woke_plants_mean , 
-                sd   = pars$woke_plants_sd) )
+                sd   = pars$woke_plants_sd) ) * h
 }
 
 #fecundity
-fec<-function(x,y, pars){
+fec<-function(x,y, pars, h, site){
   xb <- x_range(x, pars)
- return( flx(xb,pars)*fln(xb,pars)*pars$fruiting*nrxy(y,pars) )
+ return( flx(xb,pars, site) * fln(xb,pars, site) * pars$fruiting * nrxy(y,pars, h) )
 }
-# discretize the size distribution
-n   <- pars$mat_siz
-L   <- pars$L 
-U   <- pars$U
-#these are the upper and lower integration limits
-h   <- (U-L)/n                   #Bin size
-b   <- L+c(0:n)*h                #Lower boundaries of bins 
-y   <- 0.5*(b[1:n]+b[2:(n+1)])   #Bins' midpoints
-
-
-# Plot functions sx, flx,fln, and dox.
-tiff("function_plots.tiff", unit="cm", 
-     width=16, height=16, res=400, compression="lzw")
-
-# plot models
-par( mfrow=c(2,2), mar = c(3,3,2,0.3), mgp = c(2,0.7,0) )
-
-plot(y, sx(y,pars), main = 'survival function',
-     xlab = expression('y (that is: log size'[t]*')') )
-plot(y, flx(y,pars), main = 'flowering',
-     xlab = expression('y (that is: log size'[t]*')') )
-plot(y, fln(y,pars), main = 'flowernumber',
-     xlab = expression('y (that is: log size'[t]*')') )
-plot(y, dox(y,pars), main = 'going dormant',
-     xlab = expression('y (that is: log size'[t]*')') )
-
-dev.off()
- 
-
-# filled in and workes up to this point
-# set up a population that contains 1 individual for each y size bin
-n_vec <- rep(1, length(y) )
-
-
-# 4.How many of these 50 individuals survive from time t to time t+1? Provide the result and paste the code with which you obtained this result.
-sx(y,pars)%*%n_vec
-
 
 
 
 # IPM lambda ------------------------------------------------------------
 
 # IPM kernel
-kernel <- function(pars){
+kernel <- function(pars, site){
   
   n   <- pars$mat_siz
   L   <- pars$L 
@@ -354,7 +288,7 @@ kernel <- function(pars){
   Fmat            <- matrix(0,(n+1),(n+1))
   
   Fmat[2:(n+1),
-       2:(n+1)] <-fec(x,y,pars)
+       2:(n+1)] <-fec(y, y, pars, h, site)
   
   
  
@@ -364,18 +298,17 @@ kernel <- function(pars){
   
   # Growth/survival transitions among cts sizes
   Tmat[2:(n+1),
-       2:(n+1)]   <- t( outer(y,y,pxy,pars) )*h
-  
+       2:(n+1)]   <- t( outer(y,y,pxy,pars)* h )
   
 # Dormancy
   # living  plants go dormant and go in top row
-  Tmat[1,2:(n+1)] <-dox(x,pars)  #get in to  dormancy
+  Tmat[1,2:(n+1)] <-dox(y,pars,site)  #get in to  dormancy
   
   # dormant plants stay dormant for an other year
   Tmat[1,1]       <- pars$p_stay #stay dormant 
   
   # Graduation from dormancy to cts size
-  Tmat[2:(n+1),1] <- pars$p_out * woxy(y,pars)*h #get out of d * size distn   
+  Tmat[2:(n+1),1] <- pars$p_out * woxy(y,pars,h) #get out of d * size distn   
   
 
   
@@ -388,20 +321,40 @@ kernel <- function(pars){
               meshpts = y))
   
 }
-k_yx
-kernel(pars = pars)
+
+# there are 4 sites (numbered 1-4)
+kernel(pars, site = 1)
+
 # Deterministic lambda
-lambda <- Re(eigen(kernel(pars)$k_yx)$value[1])
 
-
-#6. Create a function that calculates deterministic lambda
-
-lamda1<-function(pars)
-{
-  Re(eigen(kernel(pars)$k_yx)$value[1])
+lambda <-function(pars, site){
+  Re(eigen(kernel(pars, site)$k_yx)$value[1])
 }
-pars$mat_siz  <- sizes[i]
-lamda1(pars)
+
+lambda(pars, site = 1)
+lambda(pars, site = 2)
+lambda(pars, site = 3)
+lambda(pars, site = 4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #7. Plot the effect of matrix (kernel) size on lambda 
