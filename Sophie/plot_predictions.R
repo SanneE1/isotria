fb498707ff718f6f35ae1f012ea746549742cbae
Sanne <sanne.evers@idiv.de>
2020-03-27 16:37:25 +0100

    update prediction 2.0

--- Sophie/plot_predictions.R
+++ Sophie/plot_predictions.R
@@ -38,9 +38,6 @@ logitbin_df <- function (df, resp, xvar, ..., n =100, log_trans_xvar = FALSE) {
   return(df)
 }
 
-# fit models --------------------------------------------------------------------------------------------------
-
-
 
 ### Create inverse logit function--------------------------------------------------------------------------------------------------
 
@@ -52,67 +49,53 @@ inv_logit<-function(x){
 x_seq <- seq(min(iso$size_t0, na.rm=T), 
              max(iso$size_t0, na.rm=T), by = 0.1)
 site <- c(1:4)
-pred_df <- expand.grid(size_t0 = x_seq, Site = site)
+pred_df <- expand.grid(size_t0 = x_seq, Site = site) %>%
+  mutate(Site = as.factor(Site))
+
 
 # create prediction values --------------------------------------------------------------------------------------------------
 #sr
-sr_b0<-fixef(sr_mod)[1]
-sr_b1<-fixef(sr_mod)[2]
 sr_y_pred<- cbind(pred_df, 
-                  pred = inv_logit(sr_b0 + sr_b1 * pred_df$size_t0))
+                  pred = predict(sr_mod, newdata = pred_df, type = "response", re.form = NA))
 
 #gr
-gr_b0<-fixef(gr_mod)[1]
-gr_b1<-fixef(gr_mod)[2]
 gr_y_pred<-cbind(pred_df,
-                 pred = (gr_b0 + gr_b1 * pred_df$size_t0)
+                 pred = predict(gr_mod, newdata = pred_df, type = "response", re.form = NA)
 )
 
 #flowpop_mod
-flp_b0<-fixef(flowpop_mod)[1]
-flp_b1<-fixef(flowpop_mod)[2]
-flp_b2 <- fixef(flowpop_mod)[3]
-flp_b3 <- fixef(flowpop_mod)[4]
 flp_y_pred<-cbind(pred_df,
-                  pred = inv_logit(flp_b0+ flp_b1 * pred_df$size_t0 + flp_b2 * pred_df$Site + flp_b3 * pred_df$Site * pred_df$size_t0)   
+                  pred = predict(flowpop_mod, newdata = pred_df, type = "response", re.form = NA)
 )
 
 #flower_n_mod
-fln_b0<-fixef(flower_n_mod)[1]
-fln_b1<-fixef(flower_n_mod)[2]
-fln_b2 <- fixef(flower_n_mod)[3]
 fln_y_pred<-cbind(pred_df,
-                  pred = exp(fln_b0+fln_b1* pred_df$size_t0 + fln_b2 * pred_df$Site)  
+                  pred =predict(flower_n_mod, newdata = pred_df, type = "response", re.form = NA) 
 )
 
 #dorm_mod
-do_b0<-fixef(dorm_mod)[1]
-do_b1<-fixef(dorm_mod)[2]
-do_b2 <- fixef(dorm_mod)[3]
-do_b3 <- fixef(dorm_mod)[4]
 do_y_pred<-cbind(pred_df,
-                 pred = inv_logit(do_b0+do_b1* pred_df$size_t0 + do_b2 * pred_df$Site + do_b3 * pred_df$Site * pred_df$size_t0) 
+                 pred = predict(dorm_mod, newdata = pred_df, type = "response", re.form = NA)
 )
 
 
 ### Plot predictions
 
 Surv <- ggplot() +
-  geom_point(data = logitbin_df(df = iso, resp = surv_t1, xvar = size_t0, Site) %>% mutate(Site = as.factor(Site)), 
-             aes(x = size_t0, y = surv_t1)) + 
+  geom_point(data = logitbin_df(df = iso, resp = surv_t1, xvar = size_t0, Site), 
+             aes(x = size_t0, y = surv_t1, colour = Site)) + 
   geom_line(data = sr_y_pred, 
-            aes(x = size_t0, y = pred), 
-            colour = "red")
+            aes(x = size_t0, y = pred, colour = Site))
 
 Growth <- ggplot() +
-  geom_point(data = iso %>% mutate(Site = as.factor(Site)), 
+  geom_point(data = iso, 
              aes(x = size_t0, y = size_t1)) + 
   geom_line(data = gr_y_pred, 
             aes(x = size_t0, y = pred), 
             colour = "red")
 
 Flwr_p <- ggplot() +
-  geom_point(data = logitbin_df(df = iso, resp = flower_t1, xvar = size_t0, Site) %>% mutate(Site = as.factor(Site)), 
+  geom_point(data = logitbin_df(df = iso, resp = flower_t1, xvar = size_t0, Site), 
              aes(x = size_t0, y = flower_t1, colour = Site)) + 
   geom_line(data = flp_y_pred %>% mutate(Site = as.factor(Site)), 
             aes(x = size_t0, y = pred, colour = Site))
@@ -124,9 +107,10 @@ Flwr_n <- ggplot() +
             aes(x = size_t0, y = pred, colour = Site))
 
 Dorm <- ggplot() +
-  geom_point(data = logitbin_df(df = iso, resp = dormancy_t1, xvar = size_t0, Site) %>% mutate(Site = as.factor(Site)), 
-             aes(x = size_t0, y = dormancy_t1, colour = Site)) + 
-  geom_line(data = do_y_pred %>% mutate(Site = as.factor(Site)), 
-            aes(x = size_t0, y = pred, colour = Site))
+  geom_point(data = logitbin_df(df = iso, resp = dormancy_t1, xvar = size_t0, Site), 
+             aes(x = size_t0, y = dormancy_t1)) + 
+  geom_line(data = do_y_pred, 
+            aes(x = size_t0, y = pred),
+            colour = 'red')
 
 (Surv + Growth) / (Flwr_p + Flwr_n) / (Dorm + plot_spacer()) + plot_layout(guides = "collect")
